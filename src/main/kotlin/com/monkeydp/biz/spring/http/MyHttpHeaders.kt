5d0b8e5b492084cfca3340570cad8fc16d4ccdbe
iPotato-Work <monkeydpotato@gmail.com>
2020-06-28 23:25:33 +0800

    *

---
+++ src/main/kotlin/com/monkeydp/biz/spring/http/MyHttpHeaders.kt
@@ -0,0 +1,10 @@
+package com.monkeydp.biz.spring.http
+
+/**
+ * @author iPotato-Work
+ * @date 2020/6/2
+ */
+object MyHttpHeaders {
+    const val AUTO_LOGIN = "autologin"
+    const val WITH_KEYS = "withKeys"
+}
