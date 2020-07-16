5a8d64b0f398be8891033706af59ca3c359a9c16
iPotato-Work <monkeydpotato@gmail.com>
2020-07-16 18:53:02 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/util/PasswordEncoder.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/util/PasswordEncoder.kt
@@ -7,15 +7,23 @@ import org.apache.commons.codec.digest.DigestUtils
  * @date 2020/6/1
  */
 interface PasswordEncoder {
-    fun encode(password: String): String
+    fun sha1Hex(password: String): String
+    fun sha256Hex(password: String): String
 
     companion object {
         operator fun invoke(): PasswordEncoder = StdPasswordEncoder()
     }
+
+    enum class Encryption {
+        SHA1, SHA256
+    }
 }
 
 private class StdPasswordEncoder : PasswordEncoder {
-    override fun encode(password: String): String =
+    override fun sha1Hex(password: String) =
             DigestUtils.sha1Hex(password)
+
+    override fun sha256Hex(password: String) =
+            DigestUtils.sha256Hex(password)
 }
 
