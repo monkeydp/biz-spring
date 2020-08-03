4144a5ca1ca3507c6a766ca869f1a7d7ef8fb03b
iPotato-Work <monkeydpotato@gmail.com>
2020-08-03 23:30:22 +0800

    *

---
+++ src/test/kotlin/com/monkeydp/biz/spring/test/BizSpringMainTest.kt
@@ -0,0 +1,14 @@
+package com.monkeydp.biz.spring.test
+
+import org.junit.jupiter.api.Test
+
+/**
+ * @author iPotato-Work
+ * @date 2020/8/3
+ */
+class BizSpringMainTest {
+    @Test
+    fun test() {
+
+    }
+}
