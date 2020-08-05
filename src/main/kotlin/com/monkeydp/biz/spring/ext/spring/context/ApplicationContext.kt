ddca6ca63542865fd2768e383cd883cd98c48120
iPotato-Work <monkeydpotato@gmail.com>
2020-08-05 22:45:21 +0800

    temp

---
+++ src/main/kotlin/com/monkeydp/biz/spring/ext/spring/context/ApplicationContext.kt
@@ -0,0 +1,10 @@
+package com.monkeydp.biz.spring.ext.spring.context
+
+import org.springframework.context.ApplicationContext
+
+/**
+ * @author iPotato-Work
+ * @date 2020/8/5
+ */
+inline fun <reified T> ApplicationContext.getBeanX(beanName: String): T =
+        getBean(beanName, T::class.java)
