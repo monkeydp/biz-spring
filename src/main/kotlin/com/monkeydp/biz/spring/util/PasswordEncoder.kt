adbb7a02ff7c8c56e5c1eb8567033f0d7804dbb5
iPotato-Work <monkeydpotato@gmail.com>
2020-08-10 15:14:02 +0800

    md5

--- src/main/kotlin/com/monkeydp/biz/spring/util/PasswordEncoder.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/util/PasswordEncoder.kt
@@ -7,19 +7,22 @@ import org.apache.commons.codec.digest.DigestUtils
  * @date 2020/6/1
  */
 interface PasswordEncoder {
+    fun md5Hex(password: String): String
     fun sha1Hex(password: String): String
     fun sha256Hex(password: String): String
 
     companion object {
-        operator fun invoke(): PasswordEncoder = StdPasswordEncoder()
+        operator fun invoke(): PasswordEncoder =
+                object : PasswordEncoder {
+                    override fun md5Hex(password: String) =
+                            DigestUtils.md5Hex(password)
+
+                    override fun sha1Hex(password: String) =
+                            DigestUtils.sha1Hex(password)
+
+                    override fun sha256Hex(password: String) =
+                            DigestUtils.sha256Hex(password)
+                }
     }
-}
-
-private class StdPasswordEncoder : PasswordEncoder {
-    override fun sha1Hex(password: String) =
-            DigestUtils.sha1Hex(password)
-
-    override fun sha256Hex(password: String) =
-            DigestUtils.sha256Hex(password)
 }
 
