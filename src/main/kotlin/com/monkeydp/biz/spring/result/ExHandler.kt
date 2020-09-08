cd2cca13fdc1074a7dd76894062330fc275ade4e
iPotato-Work <monkeydpotato@gmail.com>
2020-09-08 17:11:55 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/result/ExHandler.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/result/ExHandler.kt
@@ -1,13 +1,12 @@
 package com.monkeydp.biz.spring.result
 
-import com.monkeydp.biz.spring.auth.AccountNotExistEx
-import com.monkeydp.biz.spring.auth.AuthFailEx
-import com.monkeydp.biz.spring.auth.PwdIncorrectEx
 import com.monkeydp.biz.spring.ex.BizEx
 import com.monkeydp.biz.spring.result.CommonInfo.INNER_ERROR
 import com.monkeydp.biz.spring.sms.SmsBusyEx
 import com.monkeydp.biz.spring.sms.SmsSendEx
+import com.monkeydp.tools.exception.ierror
 import com.monkeydp.tools.exception.inner.InnerEx
+import com.monkeydp.tools.ext.kotlin.getMethodOrNull
 import com.monkeydp.tools.ext.kotlin.invokeMethod
 import org.springframework.validation.BindException
 import org.springframework.web.bind.MethodArgumentNotValidException
@@ -18,17 +17,16 @@ import org.springframework.web.bind.MethodArgumentNotValidException
  */
 object ExHandler {
 
-    fun handle(throwable: Throwable): FailResult =
-            ExHandler.invokeMethod<Any>(
-                    "handle",
-                    throwable
-            ) as FailResult
-
-    fun handle(ex: AccountNotExistEx): Nothing =
-            throw AuthFailEx(ex)
-
-    fun handle(ex: PwdIncorrectEx): Nothing =
-            throw AuthFailEx(ex)
+    fun handle(throwable: Throwable): FailResult {
+        val handleMethod = ExHandler.getMethodOrNull("handle", throwable)
+        if (handleMethod != null) return ExHandler.invokeMethod(handleMethod, throwable)
+        return when (throwable) {
+            is BizEx -> handle(throwable)
+            is InnerEx -> handle(throwable)
+            is Exception -> handle(throwable)
+            else -> ierror("Unhandled throwable $throwable")
+        }
+    }
 
     fun handle(ex: SmsSendEx) =
             handle(SmsBusyEx(cause = ex))
