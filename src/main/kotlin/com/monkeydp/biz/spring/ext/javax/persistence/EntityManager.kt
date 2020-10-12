1bd7b045329fc1686ef3a6d308b8799416e93e25
iPotato-Work <monkeydpotato@gmail.com>
2020-10-12 21:18:51 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/ext/javax/persistence/EntityManager.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/ext/javax/persistence/EntityManager.kt
@@ -1,10 +1,24 @@
 package com.monkeydp.biz.spring.ext.javax.persistence
 
+import javax.persistence.EntityExistsException
 import javax.persistence.EntityManager
 
 /**
  * @author iPotato-Work
  * @date 2020/5/29
  */
-fun EntityManager.persistAll(entities: Iterable<Any>) =
-        entities.forEach(this::persist)
+fun EntityManager.persistOrMerge(entity: Any) {
+    try {
+        persist(entity)
+    } catch (ex: EntityExistsException) {
+        merge(entity)
+    }
+}
+
+fun EntityManager.persistAll(entities: Iterable<Any>) {
+    entities.forEach(this::persist)
+}
+
+fun EntityManager.persistOrMergeAll(entities: Iterable<Any>) {
+    entities.forEach(this::persistOrMerge)
+}
