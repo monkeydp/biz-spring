484254e1c978aafe89a6c4e66bb8e000c58aceeb
iPotato-Work <monkeydpotato@gmail.com>
2020-11-28 16:36:39 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/result/BatchActionReturn.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/result/BatchActionReturn.kt
@@ -1,5 +1,7 @@
 package com.monkeydp.biz.spring.result
 
+import com.monkeydp.biz.spring.result.BatchResult.*
+
 /**
  * 批量操作返回
  *
@@ -7,6 +9,20 @@ package com.monkeydp.biz.spring.result
  * @date 2020/11/28
  */
 class BatchActionReturn<T>(
-        val validErrorsMap: Map<T, Iterable<ValidError>>,
-        val successData: Iterable<T> = emptyList(),
-)
+        val validErrorsMap: Map<T, Iterable<ValidError>> = emptyMap(),
+        val successData: Collection<T> = emptyList(),
+) {
+    val result = when {
+        validErrorsMap.size == 0 && successData.size == 0 -> NO_DATA
+        validErrorsMap.size == 0 -> SUCCESS
+        successData.size == 0 -> FAIL
+        else -> PARTIAL_FAIL
+    }
+}
+
+enum class BatchResult {
+    NO_DATA,        // 无数据
+    SUCCESS,        // 成功
+    FAIL,           // 失败
+    PARTIAL_FAIL    // 部分失败
+}
