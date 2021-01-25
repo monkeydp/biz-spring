89af07c1f101c491001ebdb13e18be82cdb97362
iPotato-Work <monkeydpotato@gmail.com>
2021-01-25 11:25:02 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/ext/spring/context/ApplicationContext.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/ext/spring/context/ApplicationContext.kt
@@ -6,8 +6,11 @@ import org.springframework.context.ApplicationContext
  * @author iPotato-Work
  * @date 2020/8/5
  */
-inline fun <reified T> ApplicationContext.getBeanX(beanName: String): T =
+inline fun <reified T> ApplicationContext.getBeanByName(beanName: String): T =
         getBean(beanName, T::class.java)
 
-inline fun <reified T> ApplicationContext.getBeans() =
+inline fun <reified T> ApplicationContext.getBeanByType(): T =
+        getBean(T::class.java)
+
+inline fun <reified T> ApplicationContext.getBeansByType() =
         getBeansOfType(T::class.java).toMap()
