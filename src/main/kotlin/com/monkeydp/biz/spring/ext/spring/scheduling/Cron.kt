ebbf701f775b06f249c546d95ef22b5b9baf88bd
iPotato-Work <monkeydpotato@gmail.com>
2021-01-26 11:10:05 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/ext/spring/scheduling/Cron.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/ext/spring/scheduling/Cron.kt
@@ -1,18 +1,19 @@
 package com.monkeydp.biz.spring.ext.spring.scheduling
 
-import com.monkeydp.tools.ext.java.yearx
+import com.monkeydp.tools.module.cron.Cron
 import org.springframework.scheduling.TaskScheduler
 import org.springframework.scheduling.support.CronTrigger
-import java.text.SimpleDateFormat
-import java.util.*
 
 /**
  * @author iPotato-Work
  * @date 2020/8/10
  */
-fun TaskScheduler.scheduleCronTask(cronExp: String, task: () -> Unit) =
+fun TaskScheduler.scheduleCronTask(cronExp: CharSequence, task: () -> Unit) =
         schedule(object : Runnable {
             override fun run() {
                 task()
             }
-        }, CronTrigger(cronExp))
+        }, CronTrigger(cronExp.toString()))
+
+fun TaskScheduler.scheduleCronTask(cron: Cron, task: () -> Unit) =
+        scheduleCronTask(cron.expression, task)
