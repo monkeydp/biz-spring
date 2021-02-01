40297761d17ebb7c2aa0b004c80ebc137616760f
iPotato-Work <monkeydpotato@gmail.com>
2021-02-01 19:46:49 +0800

    *

--- src/main/kotlin/com/monkeydp/biz/spring/ext/spring/data/jpa/JPASpecificationDSL.kt
+++ src/main/kotlin/com/monkeydp/biz/spring/ext/spring/data/jpa/JPASpecificationDSL.kt
@@ -1,7 +1,13 @@
 package com.monkeydp.biz.spring.ext.spring.data.jpa
 
+import au.com.console.jpaspecificationdsl.get
 import au.com.console.jpaspecificationdsl.isFalse
 import au.com.console.jpaspecificationdsl.isTrue
+import au.com.console.jpaspecificationdsl.where
+import org.springframework.data.jpa.domain.Specification
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.Path
+import javax.persistence.criteria.Predicate
 import kotlin.reflect.KProperty1
 
 /**
@@ -10,3 +16,17 @@ import kotlin.reflect.KProperty1
  */
 fun <T> KProperty1<T, Boolean?>.`is`(boolean: Boolean) =
         if (boolean) isTrue() else isFalse()
+
+fun <T, R : Comparable<R>> KProperty1<T, R?>.compare(x: R, operator: String = "=") =
+        spec {
+            when (operator) {
+                "=" -> equal(it, x)
+                ">" -> greaterThan(it, x)
+                "<" -> lessThan(it, x)
+                "!=" -> notEqual(it, x)
+                else -> error("Unhandle operator `$operator`!")
+            }
+        }
+
+private fun <T, R> KProperty1<T, R?>.spec(makePredicate: CriteriaBuilder.(path: Path<R>) -> Predicate): Specification<T> =
+        let { property -> where { root -> makePredicate(root.get(property)) } }
